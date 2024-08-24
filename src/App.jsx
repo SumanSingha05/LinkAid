dc6642e4174b6aa1f8266fd65cc9f8557ca65390
shrestha <shresthachakrabortykgp@gmail.com>
2024-08-24 11:55:54 +0530

    chnages in app

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import LandingPage from "./Components/LandingPage";
+import SignIn from "./Components/SignIn";
+import Issues from "./Components/Issues";
+import Solution from "./Components/Solution";
+import Details from "./Components/Details";
+import SignUp from "./Components/SignUp";
+
+const App = () => {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/signIn" element={<SignIn />} />
+        <Route path="/signUp" element={<SignUp />} />
+        <Route path="/details" element={<Details />} />
+        <Route path="/issues" element={<Issues />} />
+        <Route path="/solution" element={<Solution />} />
+      </Routes>
+    </>
+  );
+};
+
+export default App;
