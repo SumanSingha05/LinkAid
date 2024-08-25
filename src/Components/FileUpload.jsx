917a6a1aa4385010a942d1da57a4f29d4d30938f
SumanSingha05 <sumansingha8654@gmail.com>
2024-08-25 14:02:23 +0530

    Styling UI Done

--- src/Components/FileUpload.jsx
+++ src/Components/FileUpload.jsx
@@ -19,8 +19,8 @@ const FileUpload = () => {
   return (
 
     <div>
-      <input className='ml-10 mb-5' type="file" onChange={(e) => setFileUpload(e.target.files[0])} />
-      <button className='border-black border-2 border-solid ml-20 mt-4 mb-2 p-2 pl-4 pr-4 rounded-full bg-[#413176] text-[#ffffff] border-none ' onClick={uploadFile}>Upload File</button>
+      <input className='sm:ml-10 ml-28 sm:mb-5' type="file" onChange={(e) => setFileUpload(e.target.files[0])} />
+      <button className='border-black border-2 border-solid sm:ml-20 sm:mt-4 sm:mb-2 mt-5 ml-32 p-2 pl-4 pr-4 rounded-full bg-[#413176] text-[#ffffff] border-none ' onClick={uploadFile}>Upload File</button>
     </div>
   )
 }
